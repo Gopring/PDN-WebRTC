f967af2d8b6beb8effc9d1b36908cca829b0d972
Changyu Moon <121847433+window9u@users.noreply.github.com>
2024-12-02 13:46:08 +0900

    refactor controller

--- signal/controller/controller.go
+++ signal/controller/controller.go
@@ -3,10 +3,9 @@ package controller
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"log"
-	"net/http"
 	"pdn/broker"
-	"pdn/pkg/socket"
 	"pdn/types/api/request"
 	"pdn/types/api/response"
 	"pdn/types/message"
@@ -17,61 +16,39 @@ const (
 	PULL = "PULL"
 )
 
-// SocketController handles HTTP requests.
-type SocketController struct {
+// Controller handles HTTP requests.
+type Controller struct {
 	broker *broker.Broker
 }
 
-// New creates a new instance of SocketController.
-func New(b *broker.Broker) *SocketController {
-	return &SocketController{
+// New creates a new instance of Controller.
+func New(b *broker.Broker) *Controller {
+	return &Controller{
 		broker: b,
 	}
 }
 
 // Process handles HTTP requests.
-func (c *SocketController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	s, err := socket.New(w, r)
-	if err != nil {
-		log.Printf("Failed to create WebSocket: %v", err)
-		return
-	}
-	defer func() {
-		if err := s.Close(); err != nil {
-			log.Printf("Failed to close WebSocket: %v", err)
-		}
-	}()
-
-	channelID, userID, err := c.authenticate(s)
+func (c *Controller) Process(socket *websocket.Conn) {
+	channelID, userID, err := c.authenticate(socket)
 	if err != nil {
 		return
 	}
-
-	// 01. Register client to broker
-	if err := c.broker.Register(broker.CLIENT, broker.FROM(channelID+userID)); err != nil {
-		log.Printf("Failed to publish to broker: %v", err)
-		return
-	}
-	defer func() {
-		if err := c.broker.Unregister(broker.CLIENT, broker.FROM(channelID+userID)); err != nil {
-			log.Printf("Failed to unregister broker: %v", err)
-		}
-	}()
 
 	// 02. subscribe itself
-	go c.response(s, channelID, userID)
+	go c.sendResponse(socket, channelID, userID)
 
-	// 03. send message to broker
-	if err := c.publish(s, channelID, userID); err != nil {
+	// 03. sendResponse message to broker
+	if err := c.receiveRequest(socket, channelID, userID); err != nil {
 		return
 	}
 }
 
-func (c *SocketController) authenticate(s socket.Socket) (string, string, error) {
+func (c *Controller) authenticate(s *websocket.Conn) (string, string, error) {
 	var req request.Activate
 
 	if err := s.ReadJSON(&req); err != nil {
-		log.Printf("Failed to read activation request: %v", err)
+		log.Printf("Failed to read activation receiveRequest: %v", err)
 		return "", "", err
 	}
 
@@ -82,32 +59,14 @@ func (c *SocketController) authenticate(s socket.Socket) (string, string, error)
 	}
 
 	if err := s.WriteJSON(res); err != nil {
-		log.Printf("Failed to write response: %v", err)
+		log.Printf("Failed to sendResponse sendResponse: %v", err)
 		return "", "", err
 	}
 
 	return req.ChannelID, req.UserID, nil
 }
 
-func (c *SocketController) response(s socket.Socket, channelID, userID string) {
-	ch, err := c.broker.Subscribe(broker.CLIENT, broker.FROM(channelID+userID))
-	if err != nil {
-		log.Printf("Failed to subscribe to broker: %v", err)
-		return
-	}
-	for {
-		msg, ok := <-ch
-		if !ok {
-			return
-		}
-		if err := s.WriteJSON(msg); err != nil {
-			log.Printf("Failed to write message: %v", err)
-			return
-		}
-	}
-}
-
-func (c *SocketController) publish(s socket.Socket, channelID, userID string) error {
+func (c *Controller) receiveRequest(s *websocket.Conn, channelID, userID string) error {
 	for {
 		// 01. read message from client
 		var req request.Signal
@@ -116,27 +75,41 @@ func (c *SocketController) publish(s socket.Socket, channelID, userID string) er
 			return err
 		}
 
-		// 02. publish message to broker
+		// 02. receiveRequest message to broker
 		switch req.Type {
 		case PUSH:
-			if err := c.broker.Publish(broker.PUSH, broker.CONTROLLER, message.Push{
+			if err := c.broker.Publish(broker.ClientMessage, broker.PUSH, message.Push{
 				ChannelID: channelID,
 				UserID:    userID,
 				SDP:       req.SDP,
 			}); err != nil {
-				log.Printf("Failed to publish to broker: %v", err)
+				log.Printf("Failed to receiveRequest to broker: %v", err)
 			}
 		case PULL:
-			if err := c.broker.Publish(broker.PULL, broker.CONTROLLER, message.Pull{
+			if err := c.broker.Publish(broker.ClientMessage, broker.PULL, message.Pull{
 				ChannelID: channelID,
 				UserID:    userID,
 				SDP:       req.SDP,
 			}); err != nil {
-				log.Printf("Failed to publish to broker: %v", err)
+				log.Printf("Failed to receiveRequest to broker: %v", err)
 			}
 		default:
 			return fmt.Errorf("invalid type: %s", req.Type)
 		}
 
+	}
+}
+
+func (c *Controller) sendResponse(s *websocket.Conn, channelID, userID string) {
+	ch := c.broker.Subscribe(broker.ClientSocket, broker.Detail(channelID+userID))
+	for {
+		msg, ok := <-ch
+		if !ok {
+			return
+		}
+		if err := s.WriteJSON(msg); err != nil {
+			log.Printf("Failed to sendResponse message: %v", err)
+			return
+		}
 	}
 }
