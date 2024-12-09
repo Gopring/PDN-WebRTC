ad28dab2963d4d6b302eb6d7c14d4e920289362f
Changyu Moon <121847433+window9u@users.noreply.github.com>
2024-12-09 19:03:01 +0900

    fix default peer connection mode

--- coordinator/config.go
+++ coordinator/config.go
@@ -3,7 +3,7 @@ package coordinator
 // Default values for the coordinator. If the values are not set, these values are used.
 const (
 	DefaultMaxForwardingNumber = 1
-	DefaultSetPeerConnection   = true
+	DefaultSetPeerConnection   = false
 )
 
 // Config contains the configuration for the coordinator.
