a0cc909d021a4aaa2a439ef3a6c122df92c228a0
jhmin99 <mjh012399@gmail.com>
2025-01-02 15:27:54 +0900

    Extend ClientInfo with enhanced fields and methods for forwarder management

--- database/client_info.go
+++ database/client_info.go
@@ -3,7 +3,7 @@ package database
 import "time"
 
 const (
-	// Candidate is the class of candidate. This means who not classified class yet.
+	// Candidate is the class of a newly connected user.
 	Candidate = iota
 
 	// Publisher is the class of publisher. This means who publishes the stream.
@@ -14,27 +14,57 @@ const (
 
 	// Fetcher is the class of fetcher. This means who fetches the stream.
 	Fetcher
+
+	// PotentialForwarder is the class of a user who might be eligible to become a forwarder.
+	PotentialForwarder
 )
 
 // ClientInfo is a struct for client information.
 type ClientInfo struct {
-	ID        string
-	ChannelID string
-	Class     int
-	CreatedAt time.Time
+	ID              string
+	ChannelID       string
+	Class           int
+	ConnectionCount int
+	CreatedAt       time.Time
+	LastUpdated     time.Time
+	//NetworkUsage   float64   // Network usage (e.g., Mbps)
+	//CPUUsage       float64   // CPU usage (Percentage)
+	//PacketLossRate float64   // Packet loss rate (Percentage)
+}
+
+// UpdateLastUpdated updates the LastUpdated field to the current time.
+func (u *ClientInfo) UpdateLastUpdated() {
+	u.LastUpdated = time.Now()
+}
+
+// IncreaseConnectionCount increases the ConnectionCount field.
+func (u *ClientInfo) IncreaseConnectionCount() {
+	u.ConnectionCount = u.ConnectionCount + 1
+}
+
+// DecreaseConnectionCount decreases the ConnectionCount field.
+func (u *ClientInfo) DecreaseConnectionCount() {
+	u.ConnectionCount = u.ConnectionCount - 1
+}
+
+// UpdateClass updates the class field with the provided value
+func (u *ClientInfo) UpdateClass(class int) {
+	u.Class = class
 }
 
 // CanForward returns whether the client can forward the stream.
 func (u *ClientInfo) CanForward() bool {
-	return u.Class == Candidate || u.Class == Forwarder
+	return u.Class == Candidate || u.Class == Forwarder || u.Class == PotentialForwarder
 }
 
 // DeepCopy creates a deep copy of the given ClientInfo.
 func (u *ClientInfo) DeepCopy() *ClientInfo {
 	return &ClientInfo{
-		ID:        u.ID,
-		ChannelID: u.ChannelID,
-		Class:     u.Class,
-		CreatedAt: u.CreatedAt,
+		ID:              u.ID,
+		ChannelID:       u.ChannelID,
+		CreatedAt:       u.CreatedAt,
+		Class:           u.Class,
+		ConnectionCount: u.ConnectionCount,
+		LastUpdated:     u.LastUpdated,
 	}
 }
